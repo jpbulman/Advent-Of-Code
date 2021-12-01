9143383bdb3f1726e9c1a40ab92af9762691146c
JP Bulman <jpbulman@wpi.edu>
2021-12-01 00:26:08 -0500

    Day 1 2021

--- 2021/day1/Program.cs
+++ 2021/day1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace day1
 {
@@ -8,10 +9,35 @@ namespace day1
         static void Main(string[] args)
         {
             var lines = File.ReadLines("input.txt");
+
+            int[] nums =  new int[lines.Count()];
+
+            int i = 0;
             foreach (var line in lines)
             {
-                Console.WriteLine(line);
+                int lineAsNum = Int32.Parse(line);
+                nums[i++] = lineAsNum;
             }
+
+            int count = 0;
+            int idx = 0;
+            foreach (int j in nums) {
+                if (idx < 3) {
+                    idx++;
+                    continue;
+                }
+
+                int currSum = nums[idx] + nums[idx - 1] + nums[idx - 2];
+                int prevSum = nums[idx - 1] + nums[idx - 2] + nums[idx - 3];
+                if (currSum > prevSum) {count++;}
+                idx++;
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static int aocMethod(int a) {
+            return 1;
         }
     }
 }
